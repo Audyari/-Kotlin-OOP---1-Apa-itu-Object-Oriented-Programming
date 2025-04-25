daafb28938904543329cf3bcad5b330f5062a2af
Audyari Wiyono <audy123ari@gmail.com>
2025-04-25 11:24:20 +0700

    membuat Object

---
+++ Belajar Kotlin OOP/src/app/Object.kt
@@ -0,0 +1,9 @@
+import data.Person
+
+fun main(){
+    val audy = Person()
+    val wiyo = Person()
+
+    println(audy)
+    println(wiyo)
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 "# -Kotlin-OOP---1-Apa-itu-Object-Oriented-Programming" 
+
+1. Membuat Class
+2. Membuat Object
