beb4c8c6a6a01f7312122741030f568ec85b377f
Audyari WIyono <Audy123ari@gmail.com>
2025-04-26 21:45:19 +0700

    di ganggu istri

---
+++ Belajar Kotlin OOP/src/app/FunctionOverriding.kt
@@ -0,0 +1,13 @@
+package app
+
+import data.Employee
+
+
+fun main(){
+
+    val func = Employee("AI")
+
+    func.sayHello("Audyari Manager")
+
+    func.sayHello("Audyari Vice President")
+}
--- Belajar Kotlin OOP/src/data/Employee.kt
+++ Belajar Kotlin OOP/src/data/Employee.kt
@@ -8,6 +8,15 @@ open class Employee(
         println("Hello $name my name is ${this.name}, I'm an Employee")
     }
 }
-
-final class Manager(name: String) : Employee(name)
-final class VicePresident(name: String) : Employee(name)
+final class Manager(name: String) : Employee(name){
+    // 14. Function Overriding
+    override fun sayHello(name: String) {
+        println("Hello $name my name is ${this.name}, I'm a Manager")
+    }
+}
+final class VicePresident(name: String) : Employee(name){
+    // 14. Final Override Function
+    override fun sayHello(name: String) {
+        println("Hello $name my name is ${this.name}, I'm a Vice President")
+    }
+}
